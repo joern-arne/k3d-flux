1252f48915166d29603129a96210e7e42846da51
Jörn Arne Göttig <joern.goettig@lhind.dlh.de>
2023-10-01 14:47:38 +0200

    Prepare secrets

--- secrets/secrets_generator.py
+++ secrets/secrets_generator.py
@@ -1,25 +1,85 @@
 #!/usr/bin/env python3
 
-import yaml
-import json
+import yaml, json
+import subprocess
 
-RELATIVE_PATH = 'secrets'
-COMMAND_TEMPLATE = '''
-op item create --category=login
-\t--vault={vault}
-\t--title={title}
-\t{kv_pairs}
-'''
+def load_config(location='secrets/k3d-dev.secrets.yaml'):
+    with open(location, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    return config.get('vault'), config.get('secrets')
 
-with open(f'{RELATIVE_PATH}/k3d-dev.secrets.yaml', 'r') as file:
-    config = yaml.safe_load(file)
 
-vault = config.get('vault')
-secrets = config.get('secrets')
 
-for secret in secrets:
-    print(COMMAND_TEMPLATE.format(
-        vault=vault,
-        title=secret.get('title'),
-        kv_pairs='\n\t'.join(list(map(lambda v: f'"{v[0]}={v[1]}"', secret.get('data').items())))
-    ))
+def op_exists(vault, secret):
+    print(secret.get('title')+' ', end='')
+    s = subprocess.Popen(
+        'op item get {title} --vault={vault}'.format(
+            vault=vault,
+            title=secret.get('title')
+        ).split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    streamdata = s.communicate()[0]
+
+    if s.returncode == 0:
+        print('exists')
+        return True
+    else:
+        print('does not exist')
+        return False
+
+
+
+def op_create(vault, secret):
+    print(secret.get('title'), 'create')
+    
+    s = subprocess.Popen(
+        'op item create --category=login --vault={vault} --title={title} {kv_pairs}'.format(
+            vault=vault,
+            title=secret.get('title'),
+            kv_pairs='\n\t'.join(list(map(lambda v: f'"{v[0]}={v[1]}"', secret.get('data').items())))
+        ).split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    streamdata = s.communicate()[0]
+
+
+def op_get_list(vault):
+    s = subprocess.Popen(
+        'op item list --vault {vault} --format=json'.format(
+            vault=vault,
+        ).split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    streamdata = s.communicate()[0]
+    return json.loads(streamdata)
+
+def op_delete(vault, secret):
+    pass
+
+def op_clean(vault, secrets):
+    
+    titles = [item.get('title') for item in secrets]
+    print(titles)
+
+    contents = op_get_list(vault)
+    
+    for title in [item.get('title') for item in contents]:
+        if title not in titles:
+
+    
+
+if __name__ == '__main__':
+
+    vault, secrets = load_config()
+    
+    # for secret in secrets:
+    #     if not op_exists(vault, secret):
+    #         op_create(vault, secret)
+
+    op_clean(vault, secrets)
+
