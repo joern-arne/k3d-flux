1ba4d53ec77dbf3034d3d671ddc543fded86095c
Jörn Arne Göttig <joern.goettig@lhind.dlh.de>
2023-10-01 15:30:57 +0200

    Prepare secrets

--- secrets/secrets_generator.py
+++ secrets/secrets_generator.py
@@ -24,27 +24,29 @@ def op_exists(vault, secret):
     streamdata = s.communicate()[0]
 
     if s.returncode == 0:
-        print('exists')
+        print(f'exists in {vault}')
         return True
     else:
-        print('does not exist')
+        print(f'does not exist in {vault}')
         return False
 
 
 
 def op_create(vault, secret):
-    print(secret.get('title'), 'create')
-    
+    print(f'create {secret.get("title")} in {vault}')
+
     s = subprocess.Popen(
-        'op item create --category=login --vault={vault} --title={title} {kv_pairs}'.format(
+        'op item create --category=login --vault={vault} --title={title} {tags} {kv_pairs}'.format(
             vault=vault,
             title=secret.get('title'),
-            kv_pairs='\n\t'.join(list(map(lambda v: f'"{v[0]}={v[1]}"', secret.get('data').items())))
+            kv_pairs='\n\t'.join(list(map(lambda v: f'"{v[0]}={v[1]}"', secret.get('data').items()))),
+            tags='--tags ' + ','.join(secret.get('tags')) if secret.get('tags') else ''
         ).split(),
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
     streamdata = s.communicate()[0]
+
 
 
 def op_get_list(vault):
@@ -58,28 +60,36 @@ def op_get_list(vault):
     streamdata = s.communicate()[0]
     return json.loads(streamdata)
 
-def op_delete(vault, secret):
-    pass
+
+
+def op_delete(vault, title):
+    print(f'remove {title} from {vault}')
+    s = subprocess.Popen(
+        'op item delete {title} --vault {vault}'.format(
+            vault=vault,
+            title=title
+        ).split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    streamdata = s.communicate()[0]
+
+
 
 def op_clean(vault, secrets):
-    
     titles = [item.get('title') for item in secrets]
-    print(titles)
-
     contents = op_get_list(vault)
     
     for title in [item.get('title') for item in contents]:
         if title not in titles:
+            op_delete(vault, title)
 
-    
+
 
 if __name__ == '__main__':
-
     vault, secrets = load_config()
-    
-    # for secret in secrets:
-    #     if not op_exists(vault, secret):
-    #         op_create(vault, secret)
+    for secret in secrets:
+        if not op_exists(vault, secret):
+            op_create(vault, secret)
 
     op_clean(vault, secrets)
-
